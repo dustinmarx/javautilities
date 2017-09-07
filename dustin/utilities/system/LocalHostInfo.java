5d6e3b8d4bd071e3cc8607a511890d2031cccf37
Dustin <dustinmarx@gmail.com>
2017-09-06 20:15:15 -0600

    Minor updates to LocalHostInfo class
    Remove extraneous comments on checked exceptions to leave simpler code that  demonstrates InetAddress.

--- dustin/utilities/system/LocalHostInfo.java
+++ dustin/utilities/system/LocalHostInfo.java
@@ -1,14 +1,14 @@
 package dustin.utilities.system;
 
+import static java.lang.System.out;
+
 import java.net.InetAddress;
-//import java.net.UnknownHostException;
+import java.net.UnknownHostException;
+
 /**
  * This class provides a very simple example of using the java.net.InetAddress
  * class.  It demonstrates methods on the java.net.InetAddress class such as
  * getLocalHost(), getHostName(), getHostAddress(), and getAddress().
- *
- * In addition this class demonstrates the need to catch the checked exception
- * java.net.UnknownHostException (or an exception above it such as Exception).
  *
  * Originally included with "Inspired by Actual Events" blog post "InetAddress Example"
  * (http://marxsoftware.blogspot.com/2008/01/inetaddress-example.html) and again with
@@ -21,28 +21,23 @@ public class LocalHostInfo
     * Run simple demonstration of the usefulness of the java.net.InetAddress
     * class.
     *
-    * @param aArgs The command line arguments; none expected.
+    * @param arguments The command line arguments; none expected.
     */
-   public static void main(final String[] aArgs)
+   public static void main(final String[] arguments)
    {
-      InetAddress localhost = null;
       try
       {
-         localhost = InetAddress.getLocalHost();
-         System.out.println( "InetAddress: " + localhost );
-         System.out.println( "\ttoString: " + localhost.toString() );
-         System.out.println(  "\tCanonicalHostName: "
+         final InetAddress localhost = InetAddress.getLocalHost();
+         out.println( "InetAddress: " + localhost );
+         out.println( "\ttoString: " + localhost.toString() );
+         out.println(  "\tCanonicalHostName: "
             + localhost.getCanonicalHostName() );
-         System.out.println( "\tHost Name: " + localhost.getHostName() );
-         System.out.println( "\tHost Address: " + localhost.getHostAddress() );
-         System.out.println( "\tHost Bytes: " + localhost.getAddress() );
-         System.out.println( "\tHash Code: " + localhost.hashCode() );
-
-         // We will now force an UnknownHostException to be thrown.
-         InetAddress nonExistent = InetAddress.getByName("nada");
-         System.out.println( "Host is: " + nonExistent );  // won't get here!
+         out.println( "\tHost Name: " + localhost.getHostName() );
+         out.println( "\tHost Address: " + localhost.getHostAddress() );
+         out.println( "\tHost Bytes: " + localhost.getAddress() );
+         out.println( "\tHash Code: " + localhost.hashCode() );
       }
-      catch (/*UnknownHost*/Exception unknownHostException) // checked exception
+      catch (UnknownHostException unknownHostException)
       {
          System.err.println(  "D'oh!! Unknown Host ("
             + unknownHostException.getClass().toString()
